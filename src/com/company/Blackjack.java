d7a84a043eaf1b8f8e03f922c089590522cfa942
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-10-29 01:13:54 -0500

    Working first draft

--- src/com/company/Blackjack.java
+++ src/com/company/Blackjack.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Blackjack {
 
     public static void main(String[] args) {
-	// write your code here
+        int card, total;
+        char playAgain, anotherCard = 'y';
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        do {
+            total = 0;
+            card = random.nextInt(10) + 1;
+            total += card;
+            System.out.print("First cards: " + card + ", ");
+            card = random.nextInt(10) + 1;
+            total += card;
+            System.out.println(card);
+            System.out.println("Total: " + total);
+
+            while ((total < 21) && (anotherCard != 'n')) {
+                System.out.print("Do you want another card? (y/n): ");
+                anotherCard = input.next().charAt(0);
+
+                if (anotherCard == 'y') {
+                    card = random.nextInt(10) + 1;
+                    total += card;
+                    System.out.println("Card: " + card);
+                    System.out.println("Total: " + total);
+                }
+
+                if (total > 21) {
+                    System.out.println("Bust!");
+                } else if (total == 21) {
+                    System.out.println("Blackjack!");
+                }
+            }
+
+            System.out.print("Do you want to play again? (y/n): ");
+            playAgain = input.next().charAt(0);
+
+        } while (playAgain == 'y');
     }
 }
